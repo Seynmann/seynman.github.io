14135de9c1527744fa6bb8e1a694c7ee414366c4
Seynmann <156150816+Seynmann@users.noreply.github.com>
2026-01-03 12:11:58 +0000

    Deploying to gh-pages from @ Seynmann/Seynmann.github.io@df336ae1afb2a8ebef17a62c6fd269890e93fc8a 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Seynmann/seynman.github.io",
+    "data-repo": "Seynmann/Seynmann.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
